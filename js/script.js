43a0fb796fb33b5c28492c6ef53bdb4d2195d229
sergio-sharp <sergey.shaposhnikov@phystech.edu>
2023-07-14 00:27:58 +0300

    Коммит 1

---
+++ js/script.js
@@ -0,0 +1,32 @@
+let chooseBtn = document.getElementById('choose'),
+    receiveBtn = document.getElementById('receive'),
+    heading = document.getElementsByTagName('h2')[0],
+    nameInput = document.getElementsByClassName('contactform_name')[0],
+    phoneInput = document.querySelector('.contactform_phone'),
+    mailInput = document.querySelectorAll('.contactform_mail')[0],
+    modal = document.querySelector('.modal'),
+    closeBtn = document.querySelector('.close'),
+    text = document.getElementsByName('message')[0];
+
+
+function hover(){
+    heading.textContent='Действительно все!'
+}
+
+chooseBtn.addEventListener('mouseenter', hover);
+
+chooseBtn.addEventListener('mouseleave', function(){
+    heading.textContent = 'Все включено!'
+}); 
+
+receiveBtn.addEventListener('click', function(){
+    modal.style.display = 'block';
+})
+
+closeBtn.addEventListener('click', function(){
+    modal.style.display = 'none';
+})
+
+nameInput.addEventListener('input', function(){
+   text.value = `Меня зовут ${nameInput.value}. И я хочу спросить: `;
+})
